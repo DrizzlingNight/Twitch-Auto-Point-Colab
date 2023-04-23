1cba5059bf3bdcff62f26a28a92a08b2717789f9
DrizzlingNight <lonelywish0329@gmail.com>
2023-04-23 16:39:15 +0800

    優化代碼

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ import {followingStreamserList} from "./followingStreamserList"
 const puppeteer = require("puppeteer");
 const cmdArgs = require('command-line-args');
 const cmdUsage = require('command-line-usage');
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
 const fs = require('fs').promises;
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 const usage = cmdUsage(usageOptions);
 const args = cmdArgs(cmdOptions);
@@ -20,8 +26,9 @@ var streamers = new Array;
 const cheerio = require('cheerio');
 const streamersUrl = 'https://www.twitch.tv/directory/following/live'; // 2021.09.14 修改取得頻道的網址（改成追隨中的live頻道）
 const channelsQuery = 'a[data-a-target="preview-card-image-link"]'; // 2021.09.14 修改取得頻道的網址的Query
-const claimPointQuery = 'button[aria-label="領取額外獎勵"]' // 領取忠誠點數額外獎勵按鈕Query
+const claimPointQuery = 'button.kIlsPe[aria-label="領取額外獎勵"]' // 領取忠誠點數額外獎勵按鈕Query
 const pointNumberQuery = 'div[data-test-selector="balance-string"] span' // 忠誠點數數量Query
+const streamOfflineQuery = 'a[status="offline"]'; // 判斷頻道是否離線Query
 
 // 自己新增 End
 
@@ -57,7 +64,7 @@ function warn(msg: string) {
 }
 
 async function initTwitch(page: Page) {
-    info('Navigating to Twitch');
+    info('📱 Navigating to Twitch');
     await page.goto('https://twitch.tv', {
         waitUntil: ['networkidle2', 'domcontentloaded']
     });
@@ -77,20 +84,19 @@ async function initTwitch(page: Page) {
     );
 }
 
+async function isLive(mainPage: Page) {
+    const status = await mainPage.$$eval('a[status]', li => li.pop()?.getAttribute('status'));
+    const videoDuration = await mainPage.$$eval('video', videos => (videos.pop() as HTMLVideoElement)?.currentTime);
+    const raid = mainPage.url().includes('?referrer=raid');
+    vinfo(`Current url: ${mainPage.url()}`);
+    vinfo(`Channel status: ${status}`);
+    vinfo(`Video duration: ${videoDuration}`);
+    const notLive = status !== 'live' || videoDuration === 0;
+    return {videoDuration, notLive, raid};
+}
+
 let buffering = 0;
 let prevDuration = -1;
-
-async function findRandomChannel(page: Page) {
-    await page.goto(directoryUrl, {
-        waitUntil: ['networkidle2', 'domcontentloaded']
-    });
-    const aHandle = await page.waitForSelector('a[data-a-target="preview-card-image-link"]', {timeout: 0});
-    const channel = await page.evaluate(a => a.getAttribute('href'), aHandle);
-    info('Channel found: navigating');
-    await page.goto(`https://twitch.tv${channel}`, {
-        waitUntil: ['networkidle2', 'domcontentloaded']
-    });
-}
 
 // 自己新增 Start
 async function getAllStreamer(page: Page) {
@@ -133,6 +139,8 @@ async function findOnlineChannelFromList(page: Page) {
     prevDuration = -1;
     info('===從自定義的主播清單裡尋找===');
     info('Finding online channel...');
+    info("=========================");
+    info('🧹 Listing online streamers...');
     streamers = []
     for (var i = 0; i < followingStreamserList.length; i++ ) {
         const channel = followingStreamserList[i]
@@ -148,241 +156,162 @@ async function findOnlineChannelFromList(page: Page) {
             info(`/${channel} is online`);
         }
     }
+    info("=========================");
     if (!streamers[0]) {
         info('No channels online! Trying again after the timeout');
     }
     return;
 }
 
-// 領取忠誠點數
-async function checkClaimBtn(page: Page) {
-    const claimButtons = (await page.$$(claimPointQuery));
-    
-    // const pointNumberTest = (await page.$$(pointNumberQuery));
-    // for (const point of pointNumberTest) {
-    //     info(`${page.mainFrame().url()} point get！ Now point: ${point}`); // 這段之後刪掉
-    // }
-    if (claimButtons.length > 0) {
-        // const pointNumber = (await page.$$(pointNumberQuery));
-        for (const claimButton of claimButtons) {
-            await claimButton.click();
-            // info(`${page.mainFrame().url()} point get！ Now point: ${pointNumber[0]}`); // 這段之後刪掉
-        }
-    }
-}
-
-async function myRunTimer(page: Page) {
-    vinfo('myRunTimer function called')
-    await checkClaimBtn(page);
-    setTimeout(myRunTimer, 1000, page);
-}
-
-// 自己新增 End
-
-let list: string[];
-
-async function readList() {
-    info(`Parsing list of channels: ${file}`);
-    const read = await fs.readFile(file, {encoding: "utf-8"});
-    list = read.split(/\r?\n/).filter((s: string) => s.length !== 0);
-    info(`${list.length} channels found: ${list.join(', ')}`);
-}
-
-async function findChannelFromList(page: Page) {
-    if (!list) await readList();
-    for (let channel of list) {
-        vinfo(`Trying ${channel}`)
-        await page.goto(`https://twitch.tv/${channel}`, {
-            waitUntil: ['networkidle2', 'domcontentloaded']
+// 獲取忠誠點數的數字
+async function getPointNumber(page: Page, pointNumberQuery: string): Promise<string> {
+    try {
+        const pointNumber = await page.$eval(pointNumberQuery, (element) => {
+            return element.textContent ? element.textContent: '0';
         });
-        const live = !(await isLive(page)).notLive;
-        vinfo(`Channel live: ${live}`);
-        if (!live) vinfo('Channel offline, trying next channel');
-        else {
-            if (game) {
-                const gameLink = await page.waitForSelector('a[data-a-target="stream-game-link"]', {timeout: 0});
-                const href = await page.evaluate(a => a.getAttribute('href'), gameLink);
-                const streamingGame = href.toLowerCase().endsWith(`/${game.toLowerCase()}`);
-                vinfo(`Channel streaming the given game: ${streamingGame}`);
-                if (!streamingGame) continue;
-            }
-            info('Online channel found!');
-            return;
-        }
+        return pointNumber;
+    } catch (error) {
+      console.error('Error during getting point number');
+      return '0';
     }
-    info('No channels online! Trying again after the timeout');
 }
 
-async function findCOnlineChannel(page: Page) {
-    buffering = 0;
-    prevDuration = -1;
-    info('Finding online channel...');
-
-    if (file) await findChannelFromList(page);
-    else await findRandomChannel(page);
-}
-
-async function checkInventory(inventory: Page) {
-    await inventory.goto('https://twitch.tv/inventory', {
+async function loopWatch(streamer: any) {
+    info(`👁‍🗨 now Watch ${streamer.link}, 🕒 Start: ${streamer.startTime}`);
+    await streamer.page.goto(`https://twitch.tv${streamer.link}`, {
         waitUntil: ['networkidle2', 'domcontentloaded']
     });
-    const claimButtons = (await inventory.$$('button[data-test-selector="DropsCampaignInProgressRewardPresentation-claim-button"]'));
-    vinfo(`${claimButtons.length} claim buttons found${claimButtons.length > 0 ? '!' : '.'}`);
-    for (const claimButton of claimButtons) {
-        info('Reward found! Claiming!')
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await claimButton.click();
-        await new Promise(resolve => setTimeout(resolve, 1000));
+    streamer.startPoint = await getPointNumber(streamer.page, pointNumberQuery);
+    while (streamer.isRun) {
+        await claimPoint(streamer);
     }
 }
 
-async function isLive(mainPage: Page) {
-    const status = await mainPage.$$eval('a[status]', li => li.pop()?.getAttribute('status'));
-    const videoDuration = await mainPage.$$eval('video', videos => (videos.pop() as HTMLVideoElement)?.currentTime);
-    const raid = mainPage.url().includes('?referrer=raid');
-    vinfo(`Current url: ${mainPage.url()}`);
-    vinfo(`Channel status: ${status}`);
-    vinfo(`Video duration: ${videoDuration}`);
-    const notLive = status !== 'live' || videoDuration === 0;
-    return {videoDuration, notLive, raid};
-}
-
-async function checkLiveStatus(mainPage: Page) {
-    const {videoDuration, notLive, raid} = await isLive(mainPage);
-    if (notLive || raid) {
-        info('Channel offline');
-        await findCOnlineChannel(mainPage);
-        return;
-    }
-    if (videoDuration === prevDuration) {
-        warn('Stream buffering or offline. If this persists a new channel will be found next cycle');
-        if (++buffering > 1) {
-            info('Channel offline or stream still buffering');
-            await findCOnlineChannel(mainPage);
-            return;
+async function claimPoint(streamer: any) {
+    if (streamer.isRun === false) return;
+    const beforeClaim = streamer.claim
+    const claimBtn = await queryOnWebsite(streamer.page, claimPointQuery);
+    if (claimBtn.length > 0) {
+        try {
+            await streamer.page.evaluate((claimPointQuery:string) => {
+                try {
+                  const button = document.querySelector(claimPointQuery);
+                  if (button) {
+                    (button as HTMLElement).click();
+                  } else {
+                    console.error('claim button not found');
+                  }
+                } catch (error) {
+                  console.error('Error during claim button click:', error);
+                }
+            },claimPointQuery);
+            // await streamer.page.reload({waitUntil: ['networkidle2', 'domcontentloaded']})
+            await streamer.page.waitForTimeout(1000);
+            const claimBtnCheck = await queryOnWebsite(streamer.page, claimPointQuery);
+            if (claimBtnCheck.length === 0) {
+                streamer.claim++;
+            }
+        } catch (error) {
+            console.error('Error during page.evaluate():', error);
         }
-    } else {
-        buffering = 0;
     }
-    prevDuration = videoDuration;
+    const afterClamim = streamer.claim
+    if (beforeClaim !== afterClamim) {
+        // console.log(
+        //     `✨ [${streamer.link}] (claim ${streamer.claim} times)`
+        // );
+    }
+    
+    await streamer.page.waitForTimeout(1000);
 }
 
-async function runTimer(mainPage: Page, inventory: Page) {
-    vinfo('Timer function called')
-    await checkInventory(inventory);
-    await checkLiveStatus(mainPage);
-    setTimeout(runTimer, timeout, mainPage, inventory);
+async function state(streamer: any) {
+    // ...
+    // ...
+    const now = dayjs(); // 獲取當前時間
+    const endTime = dayjs().format('YYYY/MM/DD HH:mm:ss');
+    const durationInMinutes = now.diff(streamer.startTime, 'minute') // 計算距離開始時間的分鐘數
+    const point = await getPointNumber(streamer.page, pointNumberQuery)
+    console.log(
+        `✨ [${streamer.link}] 🕒 End: ${endTime} Duration: ${durationInMinutes} minutes ; Point: ${streamer.startPoint} ~ ${point} (claim ${streamer.claim} times)`
+    );
 }
 
-// async function run() {
-//     info('Starting application');
-//     const browser = await puppeteer.launch({executablePath: process.env.TWITCH_CHROME_EXECUTABLE, args:['--no-sandbox']});
-//     const mainPage = (await browser.pages())[0];
-//     await mainPage.setViewport({width: 1280, height: 720})
-//     await initTwitch(mainPage);
+async function watchStreamers(mainPage:any, streamersBrowser:any) {
+    if (kind === '1') {
+      await findFollowOnlineChannel(mainPage); // 從有追蹤的頁面裡找
+    } else {
+      await findOnlineChannelFromList(mainPage); // 從List裡找
+    }
+    for (var i = 0; i < streamers.length; i++) {
+        streamersBrowser[i] = {
+            browser: null,
+            page: null,
+            link: null,
+            startTime: null,
+            startPoint: null,
+            isRun: null,
+            claim: null,
+        }
 
-//     const inventory = await browser.newPage();
-//     await inventory.setViewport({width: 1280, height: 720})
-//     await mainPage.bringToFront();
+        streamersBrowser[i].browser = await puppeteer.launch({executablePath: process.env.TWITCH_CHROME_EXECUTABLE, args:['--no-sandbox']});
+        streamersBrowser[i].page = (await streamersBrowser[i].browser.pages())[0];
 
-//     await findCOnlineChannel(mainPage);
-//     setTimeout(runTimer, timeout, mainPage, inventory);
-// }
+        await streamersBrowser[i].page.setViewport({width: 1280, height: 720})
+        await initTwitch(streamersBrowser[i].page);
+        await streamersBrowser[i].page.bringToFront();
+        
+        streamersBrowser[i].link = streamers[i]
+        streamersBrowser[i].startTime = dayjs().format('YYYY/MM/DD HH:mm:ss')
+        streamersBrowser[i].isRun = true;
+        streamersBrowser[i].claim = 0;
 
-// 自己新增 Start
+        loopWatch(streamersBrowser[i]);
+  }
+}
+  
+async function killStream(streamersBrowser:any) {
+    info("🔧  Now close browser....");
+    for (const streamer of streamersBrowser) {
+        await state(streamer);
+        streamer.isRun = false;
+        await streamer.browser.close();
+    }
+    info("=========================");
+}
+
 async function run() {
+    dayjs.tz.setDefault('Asia/Taipei') // 設置dayjs時區
     process.setMaxListeners(0); // 設置MaxListeners，0代表無限
     info('Starting application');
-    const streamersBrowser = new Array
-    const browser = await puppeteer.launch({executablePath: process.env.TWITCH_CHROME_EXECUTABLE, args:['--no-sandbox']});
+    let streamersBrowser = new Array();
+    const browser = await puppeteer.launch({
+        executablePath: process.env.TWITCH_CHROME_EXECUTABLE,
+        args: ['--no-sandbox'],
+    });
     const mainPage = (await browser.pages())[0];
-    await mainPage.setViewport({width: 1280, height: 720})
+    await mainPage.setViewport({ width: 1280, height: 720 });
     await initTwitch(mainPage);
     await mainPage.bringToFront();
-    
-    if (kind === '1'){
-        await findFollowOnlineChannel(mainPage); // 從有追蹤的頁面裡找
-    } else {
-        await findOnlineChannelFromList(mainPage); // 從List裡找
-    }
-    
-    // info("=========================");
-    // info("now Watch https://www.twitch.tv/vanilla_shironeko");
-    // await mainPage.goto(`https://www.twitch.tv/vanilla_shironeko`, {
-    //     waitUntil: ['networkidle2', 'domcontentloaded']
-    // });
 
-    // await findFollowOnlineChannel(mainPage);
+    let reload = dayjs().add(30, 'minute'); // 每30分鐘刷新一次
 
-    const streamersPage = new Array
-    const claimBtnArray = new Array
+    // 每隔一段時間檢查是否需要重新執行
+    setInterval(async () => {
+        if (dayjs().isAfter(reload)) {
+            await killStream(streamersBrowser);
+            streamersBrowser = new Array();
+            await watchStreamers(mainPage, streamersBrowser);
+            info("=========================");
+            reload = dayjs().add(30, 'minute');
+        }
+    }, 5 * 60 * 1000); // 每5分鐘檢查一次
 
-
+    // 首次執行
+    await watchStreamers(mainPage, streamersBrowser);
     info("=========================");
-    for (var i = 0; i < streamers.length; i++) {
-        streamersBrowser[i] = await puppeteer.launch({executablePath: process.env.TWITCH_CHROME_EXECUTABLE, args:['--no-sandbox']});
-        streamersPage[i] = (await streamersBrowser[i].pages())[0];
-        await streamersPage[i].setViewport({width: 1280, height: 720})
-        await initTwitch(streamersPage[i]);
-        await streamersPage[i].bringToFront();
-        const link = streamers[i]
-        info(`now Watch ${link}`);
-        await streamersPage[i].goto(`https://twitch.tv${link}`, {
-            waitUntil: ['networkidle2', 'domcontentloaded']
-        });
-
-        // 設置領取忠誠點數按鈕
-        setTimeout(myRunTimer, 1000, streamersPage[i])
-        
-    // info("=========================");
-    // for (var i = 0; i < streamers.length; i++) {
-    //     streamersPage[i] = await browser.newPage();
-    //     await streamersPage[i].setViewport({width: 1280, height: 720})
-    //     await initTwitch(streamersPage[i]);
-    //     await streamersPage[i].bringToFront();
-    //     const link = streamers[i]
-    //     info(`now Watch ${link}`);
-    //     await streamersPage[i].goto(`https://twitch.tv${link}`, {
-    //         waitUntil: ['networkidle2', 'domcontentloaded']
-    //     });
-    // }
-    // info("=========================");
-    }
-    info("=========================");
-
-    setInterval(async ()=>{
-
-        // 先把browser關掉
-        info("Now close browser....");
-        for (var i = 0; i < streamers.length; i++) {
-            await streamersBrowser[i].close()
-        }
-
-        if (kind === '1'){
-            await findFollowOnlineChannel(mainPage); // 從有追蹤的頁面裡找
-        } else {
-            await findOnlineChannelFromList(mainPage); // 從List裡找
-        }
-
-        info("=========================");
-        for (var i = 0; i < streamers.length; i++) {
-            streamersBrowser[i] = await puppeteer.launch({executablePath: process.env.TWITCH_CHROME_EXECUTABLE, args:['--no-sandbox']});
-            streamersPage[i] = (await streamersBrowser[i].pages())[0];
-            await streamersPage[i].setViewport({width: 1280, height: 720})
-            await initTwitch(streamersPage[i]);
-            await streamersPage[i].bringToFront();
-            const link = streamers[i]
-            info(`now Watch ${link}`);
-            await streamersPage[i].goto(`https://twitch.tv${link}`, {
-                waitUntil: ['networkidle2', 'domcontentloaded']
-            });
-
-            // 設置領取忠誠點數按鈕
-            setTimeout(myRunTimer, 1000, streamersPage[i])
-        }
-        info("=========================");
-    },1800000)
 }
+
 // 自己新增 End
 
 run().then(() => {
